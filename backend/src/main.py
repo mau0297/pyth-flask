516a585eccc8dd0e1f4766d4de1b601b0d51ac7e
Mauricio Solano <msolano@fusionetcorp.local>
2019-02-07 16:32:48 -0600

    Agregado de angular al proyecto

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,14 @@
 # coding=utf-8
 from flask import Flask, jsonify, request
+
+from flask_cors import CORS
+
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
+
+#creating the flask application
+app = Flask(__name__)
+CORS(app)
 
 # Creating the flask application
 
