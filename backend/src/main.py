4dbe452b3b14af5d7da347af8e72afff62ffcb2a
Mauricio Solano <msolano@fusionetcorp.local>
2019-02-08 15:02:58 -0600

    opcion para agregar más examenes

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,8 @@ from flask_cors import CORS, cross_origin
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
 
+#from .auth import AuthError, requires_auth
+
 #creating the flask application
 app = Flask(__name__)
 CORS(app)
@@ -14,9 +16,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 #Sapp.config['CORS_HEADERS'] = 'Content-Type'
 
 # if needed, generate database schema
-Base.metadata.create_all(engine)
-
-# generate database schema
 Base.metadata.create_all(engine)
 
 # start session
@@ -42,6 +41,7 @@ def get_exams():
     return jsonify(exams.data)
 
 @app.route('/exams', methods=['POST'])
+#@requires_auth
 def add_exam():
     #mount exam object
     posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
@@ -58,18 +58,33 @@ def add_exam():
     session.close()
     return jsonify(new_exam), 201
 
-if len(exams) == 0:
-    # create and persist dummy exam
-    python_exam = Exam("SQLAlchemy Exam", "Test your knowledge about SQLAlchemy.", "script")
-    python_exam2 = Exam("Prueba","Esta es una prueba de sqlAlchemy","Mauricio")
-    session.add(python_exam)
-    session.add(python_exam2)
-    session.commit()
-    session.close()
+desc = int(input("Desea agregar un examen?\n1-si\n2-No\n"))
+if desc is 1:
+    resp = 0
+# create and persist dummy exam
+    while resp is not 2:
+        x = input("Ingrese el titulo del examen:\n")
+        y = input("Ingrese la descripcion del examen:\n")
+        z = input("Ingrese su nombre:\n")
+
+        resp = int(input("Desea igresar un nuevo examen?\n1-si\n2-no\n"))
+        python_exam = Exam(x,y,z)
+        session.add(python_exam)
+        session.commit()
+        session.close()
+
+
+
+
 
     # reload exams
-    exams = session.query(Exam).all()
-
+exams = session.query(Exam).all()
+"""
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+"""
 # show existing exams
 print('### Exams:')
 for exam in exams:
